adb4d8d6cfecd6fc3719fce4d53b6363ca68da71
mohammadmubeen.khan <mohammadmubeen.khan@hcltech.com>
2026-06-22 16:53:28 +0530

    basetest

--- src/test/java/org/automation/tests/BaseTest.java
+++ src/test/java/org/automation/tests/BaseTest.java
@@ -1,4 +1,17 @@
 package org.automation.tests;
 
+import org.automation.utility.DriverFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
 public class BaseTest {
+    @BeforeMethod
+    public void setUP(){
+        DriverFactory.initDriver();
+    }
+    @AfterMethod
+    public void teardown(){
+        DriverFactory.quitDriver();
+    }
 }
